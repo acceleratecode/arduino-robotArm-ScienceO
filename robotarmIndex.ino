194ae7fdf99c3e964fba08e1f48c00a3db9b2493
Ilya Kuznetsov <ilyakuznetsov.code@gmail.com>
2017-03-08 08:06:53 -0800

    Added info desc at top

--- robotarmIndex.ino
+++ robotarmIndex.ino
@@ -1,3 +1,15 @@
+/** Arduino code used for Robot Arm competition in Science Olympiad
+* __________________________Notes _________________________________
+* Use 5V for potentiometers
+* Use 9V ideally for Potentiometers
+* For the breadboard, 1 side for 5V and the other for 9V, both grounds go into the 9V side
+* When looking at potentiometer from the front (turn wheel facing towards you)
+  left plug is positive(+), middle plug is value(a), and right plug is negative/ground(-)
+* !!! Note, too much voltage can fry the breadboard or melt the wire !!!
+* Plug Analog into the potentiometer, and use the corresponding potPin value for output to Motors
+* For type of potentiometer, the Bs are better than the As due to free space on the As
+*/
+
 #include <Servo.h>
 
 Servo servo1; // base rotator servo
@@ -21,6 +33,7 @@ int valD, oldValD = 0;
 int valE, oldValE = 0;
 int valF, oldValF = 0;
 
+// initial function to initialize values
 void setup() {
   servo1.attach(2);           // setup the pin the servo data signal is on
   servo2.attach(3);
@@ -29,7 +42,7 @@ void setup() {
   servo5.attach(6);
   servo6.attach(7);     
 }
-
+// loops forever until reset button is pressed or unplugged from power source
 void loop() {
   valA = analogRead(potPin1);
   valA = map(valA, 0, 1023, 0, 180);
@@ -53,5 +66,6 @@ void loop() {
   
   delay(15);                        // delay of 15 ms for the servo to catch up
 
-  Serial.println("Servo 1: " + String(valA) + ", Servo 2: " + String(valB) + ", Servo 3: " + String(valC) + ", Servo 4: " + String(valD) + ", Servo 5: " + String(valE));
+  // used to check for output based off potentiometer
+  // Serial.println("Servo 1: " + String(valA) + ", Servo 2: " + String(valB) + ", Servo 3: " + String(valC) + ", Servo 4: " + String(valD) + ", Servo 5: " + String(valE));
 }
